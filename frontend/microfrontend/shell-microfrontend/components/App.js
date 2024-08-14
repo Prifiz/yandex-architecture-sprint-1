f45db53bf1e366160e96b82fa2c55b741379948d
Prifiz <baranov.v.e@yandex.ru>
2024-08-14 16:35:09 +0400

    Решение заданий 1-го спринта

--- frontend/microfrontend/shell-microfrontend/components/App.js
+++ frontend/microfrontend/shell-microfrontend/components/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Route, useHistory, Switch } from "react-router-dom";
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
-import PopupWithForm from "./PopupWithForm";
-import ImagePopup from "./ImagePopup";
-import api from "../utils/api";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import EditProfilePopup from "./EditProfilePopup";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Footer from "./Footer.js";
+import PopupWithForm from "../../profile-microfrontend/src/components/PopupWithForm.js";
+import ImagePopup from "./ImagePopup.js";
+import api from "../utils/api.js";
+import { CurrentUserContext } from "../../shared-lib/src/contexts/CurrentUserContext.js";
+import EditProfilePopup from "../../profile-microfrontend/src/components/EditProfilePopup.js";
 import EditAvatarPopup from "./EditAvatarPopup";
-import AddPlacePopup from "./AddPlacePopup";
-import Register from "./Register";
-import Login from "./Login";
-import InfoTooltip from "./InfoTooltip";
-import ProtectedRoute from "./ProtectedRoute";
-import * as auth from "../utils/auth.js";
+import AddPlacePopup from "../../profile-microfrontend/src/components/AddPlacePopup.js";
+import Register from "../../auth-microfrontend/src/components/Register.js";
+import Login from "../../auth-microfrontend/src/components/Login.js";
+import InfoTooltip from "../../auth-microfrontend/src/components/InfoTooltip.js";
+import ProtectedRoute from "./ProtectedRoute.js";
+import * as auth from "../../auth-microfrontend/src/utils/auth.js";
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
